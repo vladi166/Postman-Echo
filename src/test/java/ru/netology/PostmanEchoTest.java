ab6c025a8f689346131117503d81fc9acf9ec030
Vladislav Andreev <vladireft96@mail.ru>
2025-02-04 14:28:25 +0500

    build passed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
@@ -12,14 +13,14 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Hello (Привет)") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("Hello (Привет)"))
         ;
     }
 }
