6f761fb2b8ff1fc4bbdc59863639db4cf4d8ffa3
Vladislav Andreev <vladireft96@mail.ru>
2025-02-04 14:31:03 +0500

    build failed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello (Привет)"))
+                .body("date", equalTo("Hello (Привет)"))
         ;
     }
 }
